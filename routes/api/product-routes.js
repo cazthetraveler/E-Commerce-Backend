beb12c89c2ff6a0e04ebe69ba4966dbcb89fc93e
MultiMouse <olmos.luisd@outlook.com>
2023-11-20 18:52:54 -0600

    delete route for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,8 +109,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData[0]) {
+      res.status(404).json({message: "No product with this ID found!!"});
+      return;
+    }
+    res.status(204).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  };
 });
 
 module.exports = router;
