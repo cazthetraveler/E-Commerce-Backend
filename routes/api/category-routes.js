6b14c61a59f9163793f4ae2f3022a65bb3475ec3
MultiMouse <olmos.luisd@outlook.com>
2023-11-20 19:42:06 -0600

    added routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,64 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {model: Product}
+    ]
+  }).then((categoryData) => {
+    res.json(categoryData);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const categoryId = req.params.id;
+  Product.findByPk(categoryId, {
+    include: [
+      {model: Product}
+    ]
+  }).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body).then((category) => {
+    res.status(201).json(category);
+  }).catch((err) => {
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    where: req.params.id
+  }).then((updatedCategory) => {
+    res.json(updatedCategory);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({message: "No category with this ID found!!"});
+      return;
+    }
+    res.status(204).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  };
 });
 
 module.exports = router;
