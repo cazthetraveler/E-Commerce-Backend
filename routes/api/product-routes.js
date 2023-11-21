adf109374de01f6262beccaed4a8964ffbe7f67c
MultiMouse <olmos.luisd@outlook.com>
2023-11-20 18:29:05 -0600

    started on the product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {model: Category},
+      {model: Tag}
+    ]
+  }).then((productData) => {
+    res.json(productData);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const productId = req.params.id;
+  Product.findByPk(productId, {
+    include: [
+      {model: Category},
+      {model: Tag}
+    ]
+  }).then((productData) => {
+    res.json(productData);
+  });
 });
 
 // create new product
